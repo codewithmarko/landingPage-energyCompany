5c98a2243a6de9da05189fc976a76929ac071e86
unknwnmko <marko.kosic.98@gmail.com>
2023-11-07 11:21:26 +0100

    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./js/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: "Lexend",
+    },
+    extend: {
+      backgroundImage: {
+        hero: "url('/img/hero.jpg')",
+        aboutSolar: "url(/img/about/solarpanel.jpg)",
+      },
+    },
+  },
+  plugins: [],
+};
