02539e68882ed04641a09e99b6e2e28b5f8f73c5
unknwnmko <marko.kosic.98@gmail.com>
2023-11-10 12:52:29 +0100

    Update

--- main.js
+++ main.js
@@ -44,13 +44,18 @@ circleBox.forEach((box) => circleObserver.observe(box));
 
 const textContainer = document.querySelector(".textContainer");
 const textGrowContainer = document.querySelector(".textGrowContainer");
+//Big Text
 const text = document.querySelector(".textGrow");
-const textCurrentPosition = text.getBoundingClientRect();
 const styleFontSize = window
   .getComputedStyle(text, null)
   .getPropertyValue("font-size");
-
 const currentFontSize = parseFloat(styleFontSize);
+//EndPosition Text
+const textEnd = document.querySelector(".textEndPosition");
+const styleMinFontSize = window
+  .getComputedStyle(textEnd, null)
+  .getPropertyValue("font-size");
+const minFontSize = parseFloat(styleMinFontSize);
 const maxFontSize = 450;
 
 let userScrolling = false;
@@ -70,15 +75,25 @@ window.addEventListener("scroll", () => {
       const containerWidth = entry.boundingClientRect.width;
       const currentHeight = entry.boundingClientRect.top;
       const currentBottom = entry.boundingClientRect.bottom;
-      const progressAscending = (containerHeight / currentBottom).toFixed(3);
+      //Aufsteigend
+      const progressAscending =
+        1 - (currentHeight / containerHeight).toFixed(3);
+      //Absteigend
       const progressDescending = (currentHeight / containerHeight).toFixed(3);
-
-      const textEnd = document.querySelector(".textEndPosition");
+      const halfContainerHeight = containerHeight / 2;
+      const halfContainerWidth = containerWidth / 2;
+      const textCurrentPosition = text.getBoundingClientRect();
+      console.log(textCurrentPosition);
       const textEndPosition = textEnd.getBoundingClientRect();
-      const styleMinFontSize = window
-        .getComputedStyle(textEnd, null)
-        .getPropertyValue("font-size");
-      const minFontSize = parseFloat(styleMinFontSize);
+      const textEndPostionYScrollPercentage =
+        textEndPosition.top / containerHeight;
+
+      console.log(textEndPostionYScrollPercentage);
+
+      if (textEndPostionYScrollPercentage <= 0.5) {
+        textCurrentPosition.x = textEndPosition.x * progressAscending;
+        textCurrentPosition.y = textEndPosition.y * progressAscending;
+      }
 
       if (userScrolling && progressDescending >= 0 && progressDescending <= 1) {
         text.style.fontSize = `${Math.max(
@@ -87,22 +102,10 @@ window.addEventListener("scroll", () => {
         )}px`;
       }
 
-      if (textEndPosition.y <= containerHeight) {
-        text.style.transform = ` translateX(${
-          textCurrentPosition.left / -(textEndPosition.y / containerHeight) +
-          textCurrentPosition.left
-        }px)`;
-      }
-
-      // if ((textEndPosition.y / containerHeight).toFixed(2) <= 0.5) {
-      //   text.style.transform = ` translate(${
-      //     textCurrentPosition.left / -(textEndPosition.y / containerHeight) +
-      //     textCurrentPosition.left
-      //   }px,
-      //    ${
-      //      textCurrentPosition.y / -(textEndPosition.y / containerHeight) +
-      //      textCurrentPosition.top
-      //    }%)`;
+      // if (userScrolling && progressAscending <= 1 && progressAscending >= 0) {
+      //   text.style.transform = ` translateX(${
+      //     textCurrentPosition.left - halfContainerWidth * progressAscending
+      //   }px)`;
       // }
 
       //! Das ist für später wenn die Animation stimmt
@@ -129,3 +132,16 @@ window.addEventListener("scroll", () => {
 //während 2023 klein hinein scrollt wird 2023 groß kleiner
 //hier ein div mit 2-300 pixel einfügen damit es mehr zeit verschaft oder mit observer spielen
 //2023 klein bleibt picken
+
+// if (userScrolling && textEndPosition.y <= containerHeight) {
+//   text.style.transform = ` translateX(${
+//     textCurrentPosition.left / -(textEndPosition.y / containerHeight) +
+//     textCurrentPosition.left
+//   }px)`;
+// }
+
+// if (userScrolling) {
+//   text.style.transform = `translateX(${
+//     (textCurrentPosition.left * textEndPosition.y) / containerHeight
+//   }px)`;
+// }
