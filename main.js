db62f6c3f5e823ed7ab20f1cc40316e616b4593c
unknwnmko <marko.kosic.98@gmail.com>
2023-11-20 15:38:02 +0100

    cleanup

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-import './index.css';
-
-import './js/circleAnimation.js';
-import './js/textAnimation.js';
 import './css/style.css';
-import { animateCircle, circleBox } from './js/animateCircle';
-import * as animateScroll from './js/textContainerEnd.js';
-import * as progressBlock from './js/progressBlock.js';
-import * as animateVideo from './js/animateVideoOnScroll.js';
+
+import './css/style.css';
+import * as animationCircles from './js/animationCircles.js';
+import * as animationSectionOverlay from './js/animationSectionOverlay.js';
+import * as animationMissionProgress from './js/animationMissionProgress.js';
