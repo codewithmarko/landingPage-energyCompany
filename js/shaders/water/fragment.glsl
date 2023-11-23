60c6601ad2f7a9b3253a8f54bb319d5c5f673a3e
unknwnmko <marko.kosic.98@gmail.com>
2023-11-23 18:47:31 +0100

    Update

---
+++ js/shaders/water/fragment.glsl
@@ -0,0 +1,4 @@
+void main(){
+
+  gl_FragColor = vec4(95.0, 117.0, 255.0, 1.0)
+}
--- js/threejsModels.js
+++ js/threejsModels.js
@@ -1,37 +1,69 @@
-import { render } from 'sass';
 import * as THREE from 'three';
-
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+// import { waterVertexShaders } from './shaders/water/vertex.glsl';
 
+// console.log(waterVertexShaders);
 const canvas1 = document.getElementById('canvas1');
 const canvas2 = document.getElementById('canvas2');
 const canvas3 = document.getElementById('canvas3');
-const canvasTest = document.getElementById('canvasTest');
 
 class Object {
-  constructor(color, canvas) {
+  constructor(canvas, name) {
     this.experience = new Experience(canvas);
     this.scene = this.experience.scene;
+    this.textureLoader = this.experience.textureLoader;
+    this.gltfLoader = this.experience.gltfLoader;
 
-    this.setGeometry();
-    this.setMaterial(color);
-    this.setMesh();
-    this.scene.add(this.mesh);
+    this.texture = this.createTextures(name);
+    this.material = this.setMaterial(name);
+    this.loadGLTF(name);
+  }
 
-    // this.experience.tick();
+  createTextures(name) {
+    const bakedTexture = this.textureLoader.load(
+      `./models/${name}/${name}Baked.jpg`
+    );
+    bakedTexture.flipY = false;
+    bakedTexture.colorSpace = THREE.SRGBColorSpace;
+    return bakedTexture;
   }
 
   setGeometry() {
     this.geometry = new THREE.BoxGeometry(2, 2, 2);
   }
-  setMaterial(color) {
-    this.material = new THREE.MeshBasicMaterial({
-      color: color,
+  setMaterial(name) {
+    const material = new THREE.MeshBasicMaterial({ map: this.texture });
+
+    return material;
+  }
+  loadGLTF(name) {
+    this.gltfLoader.load(`./models/${name}/${name}.glb`, (gltf) => {
+      gltf.scene.traverse((child) => {
+        child.material = this.material;
+      });
+
+      const waterMesh = gltf.scene.children.find(
+        (child) => child.name === 'waterAquaPure'
+      );
+
+      if (waterMesh) {
+        this.createAdditionalMaterial(name, waterMesh);
+      }
+
+      console.log(waterMesh);
+      this.scene.add(gltf.scene);
     });
   }
-  setMesh() {
-    this.mesh = new THREE.Mesh(this.geometry, this.material);
+
+  createAdditionalMaterial(name, mesh) {
+    if (name === 'aquapure') {
+      const waterAquaPureMaterial = new THREE.MeshBasicMaterial({
+        color: 0x5f75ff,
+      });
+      mesh.material = waterAquaPureMaterial;
+    }
   }
 }
 
@@ -43,9 +75,11 @@ class Experience {
     this.camera = this.createCamera();
     this.renderer = this.createRenderer();
     this.controls = this.setControls();
+    this.textureLoader = this.createTextureLoader();
+    this.gltfLoader = this.createGLTFLoader();
 
-    window.requestAnimationFrame(() => this.tick());
     window.addEventListener('resize', () => this.resize());
+    window.addEventListener('load', () => this.requestRenderIfNotRequested());
     this.controls.addEventListener('change', () =>
       this.requestRenderIfNotRequested()
     );
@@ -69,20 +103,38 @@ class Experience {
 
   createCamera() {
     const camera = new THREE.PerspectiveCamera(
-      75,
+      65,
       this.canvas.clientWidth / this.canvas.clientHeight,
       0.1,
       1000
     );
-    camera.position.set(0, 0, 3);
+    camera.position.set(0, 8, 8);
     camera.updateProjectionMatrix();
     this.scene.add(camera);
     return camera;
   }
 
+  createTextureLoader() {
+    const textureLoader = new THREE.TextureLoader();
+    return textureLoader;
+  }
+
+  createDracoLoader() {
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath('./models/draco/');
+    return dracoLoader;
+  }
+  createGLTFLoader() {
+    const gltfLoader = new GLTFLoader();
+    return gltfLoader;
+  }
+
   setControls() {
     const controls = new OrbitControls(this.camera, this.canvas);
     controls.enableDamping = true;
+    controls.maxPolarAngle = Math.PI / 2.5;
+    controls.minDistance = 4.0;
+    controls.maxDistance = 8.0;
     return controls;
   }
 
@@ -104,15 +156,15 @@ class Experience {
   renderRequested = false;
 
   tick() {
+    console.log('tick');
     this.renderRequested = undefined;
-    console.log('animate');
     this.controls.update();
     const elapsedTime = this.clock.getElapsedTime();
     this.renderer.render(this.scene, this.camera);
   }
 
   requestRenderIfNotRequested() {
-    console.log('ASDF');
+    console.log('requestedFrame');
     if (!this.renderRequested) {
       this.renderRequested = true;
 
@@ -121,6 +173,8 @@ class Experience {
   }
 }
 
-const test = new Object(0x00ff00, canvas1);
-const test2 = new Object(0xff0000, canvas2);
-const test3 = new Object(0x0000ff, canvas3);
+//! Setting für Kamera müssen eingefühgt werden.
+
+const solara = new Object(canvas1, 'solara');
+
+const aquapure = new Object(canvas3, 'aquapure');
